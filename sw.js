51ecbbcd8ad25af0eb133d3e192c1e17c13f99e9
Kennedy Ken <122888239+Kennedy178@users.noreply.github.com>
2025-07-25 19:40:10 +0300

    fixed: congratulations message spanning outside the screen, confetti n congrat message out of sync, pwa not working on mobile

--- sw.js
+++ sw.js
@@ -2,8 +2,11 @@ const CACHE_NAME = 'daily-planner-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    'https://i.postimg.cc/x87RR692/favicon-32x32.png',
-    'https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.min.js'
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+    'https://i.postimg.cc/52PrzsyJ/favicon-32x32.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'
 ];
 
 self.addEventListener('install', event => {
