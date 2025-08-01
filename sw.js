e17a777d61bfdde7637774e32b463b474e833218
Kennedy Ken <122888239+Kennedy178@users.noreply.github.com>
2025-08-01 18:58:53 +0300

    refactoring

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('s580-routine-v1').then(cache => {
+        caches.open('getitdone-v1').then(cache => {
             return cache.addAll([
                 '/',
                 '/index.html',
